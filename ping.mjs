5150641dcaebd1e2aa29483f6b851dc90f0662fd
CC044 <CC044@users.noreply.github.com>
2024-01-05 08:30:51 +0000

    fetch and json works; mongoose needs to be fixed

--- ping.mjs
+++ ping.mjs
@@ -6,6 +6,7 @@ import express from 'express';
 import mongodb from 'mongodb';
 import mongoose from 'mongoose';
 
+
 const MongoClient = mongodb.MongoClient;
 
 const username = encodeURIComponent("user");
@@ -65,6 +66,22 @@ app.get('/index', (req, res) => {
   res.sendFile(staticPath + '/index.html');
 });
 
+app.get('/shorthtml', (req, res) => {
+  res.sendFile(staticPath + '/shorten.html');
+});
+
+app.get('/kontakt', (req, res) => {
+  res.sendFile(staticPath + '/kontakt.html');
+});
+
+app.get('/impressum', (req, res) => {
+  res.sendFile(staticPath + '/impressum.html');
+});
+
+app.get('/datenschutz', (req, res) => {
+  res.sendFile(staticPath + '/datenschutz.html');
+});
+
 /*
 app.get('/guestlist', async (req,res)=>{
   const eventId = req.query.event ;
@@ -84,6 +101,90 @@ app.get('/guestlist', async (req,res)=>{
   }
 });
 */
+
+'use strict';
+
+// Angenommen, UrlLibrary ist dein Mongoose-Modell
+// const UrlLibrary = require('./deinPfadZurUrlLibrary');
+
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+import crypto from 'crypto';
+async function generateRandomValue() {
+  const randomBytes = crypto.randomBytes(4); // Hier werden 4 Bytes verwendet, um 8 Zeichen zu erhalten
+  const randomValue = randomBytes.toString('hex').toUpperCase(); // Hexadezimal in Großbuchstaben umwandeln
+
+  return randomValue;
+}
+
+// Beispielaufruf
+const randomValue = generateRandomValue();
+console.log(randomValue);
+
+
+// Mongoose-Modelle
+const UrlLibrary = mongoose.model('urlLibrary', {
+  short_url: String,
+  long_url: String,
+  timestamp: String
+});
+const time = new Date();
+console.dir(time);
+
+// Testdaten in die Datenbank einfügen
+/*UrlLibrary.create([
+  { short_url: 'asd', long_url: 'company', timestamp: '2024-01-05T08:07:16.265Z' }
+]);*/
+
+app.post('/shorten', async (req, res) => {
+  const longUrl = req.body.longUrl;
+  const timestamp = new Date();
+  const randomValue = generateRandomValue();
+
+  // console.log(`!! req.body is...${longUrl}`);
+  res.status(200);
+  /*
+  UrlLibrary.create([
+    { short_url: randomValue, long_url: 'company', timestamp: timestamp }
+  ])
+  .then((randomValue) => {
+    res.status(200).json(randomValue);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  })*/
+});
+
+app.get('/s/:id', (req, res) => {
+  const shortUrlParam = req.params.id;
+
+  UrlLibrary.findOne({ short_url: shortUrlParam })
+    .then((eintrag) => {
+      if (!eintrag) {
+        return res.status(404).send('Eintrag nicht gefunden');
+      }
+      res.json(eintrag);
+      // res.redirect(eintrag.long_url);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Internal Server Error');
+    });
+});
+
+// Express-Routen
+app.get('/showall', (req, res) => {
+  Eintrag.find({}, (err, eintraege) => {
+    if (err) {
+      return res.status(500).send('Internal Server Error');
+    }
+    res.json(eintraege);
+  });
+});
+
+
 
 app.listen(port, (err) => {
   if (err) console.log(err);
